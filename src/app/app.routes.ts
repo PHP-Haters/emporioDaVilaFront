2a442fb0274444448c94b4cd75a1675b70957b1c
Ana Ripoli <anaripoli.eng@gmail.com>
2025-09-16 19:29:12 -0300

    pagina produtos criada e rota adicionada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PaginaInicialComponent } from './pages/pagina-inicial/pagina-inicial.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
+import { PaginaProdutosComponent } from './pages/pagina-produtos/pagina-produtos.component';
 
 export const routes: Routes = [
   { path: "", redirectTo: "/index", pathMatch: "full" },
   { path: 'index', component: PaginaInicialComponent }, 
   { path: 'login', component: LoginPageComponent }, 
+  { path: 'produtos', component: PaginaProdutosComponent }
 ];
 
 @NgModule({
