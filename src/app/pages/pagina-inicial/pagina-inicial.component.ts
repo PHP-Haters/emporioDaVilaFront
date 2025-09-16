93ad33ff663a01ad5e4637aec2ef70d383b5f8a5
João Foster <joaopfr06@gmail.com>
2025-09-15 21:05:27 -0300

    navbar adicionada

--- src/app/pages/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pages/pagina-inicial/pagina-inicial.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './pagina-inicial.component.html',
   styleUrl: './pagina-inicial.component.scss'
 })
+
 export class PaginaInicialComponent {
 
 }
