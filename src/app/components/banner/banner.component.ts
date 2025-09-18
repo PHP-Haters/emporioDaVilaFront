bdf9bac4b946781c395283558e98949e56a0fb70
Ana Ripoli <anaripoli.eng@gmail.com>
2025-09-18 20:45:12 -0300

    adicionando banner na pagina produtos

--- src/app/components/banner/banner.component.ts
+++ src/app/components/banner/banner.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from '@angular/core';
+import { NgClass } from "../../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
 
 @Component({
   selector: 'app-banner',
-  imports: [],
   templateUrl: './banner.component.html',
   styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {
+  @Input() type: 'home' | 'produtos' | 'contato' = 'home';
   @Input() imagem: string = '';
   @Input() alt: string = 'descrição da imagem';
   @Input() titulo: string = 'Seu título vai aqui';
--- src/app/pages/pagina-produtos/pagina-produtos.component.ts
+++ src/app/pages/pagina-produtos/pagina-produtos.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { BannerComponent } from '../../components/banner/banner.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-pagina-produtos',
-  imports: [ HeaderComponent ],
+  imports: [ HeaderComponent, BannerComponent, FooterComponent],
   templateUrl: './pagina-produtos.component.html',
   styleUrl: './pagina-produtos.component.scss'
 })
