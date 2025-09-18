2c781048c0fc41daa1ad015aeabafd781531d471
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-09-18 09:38:55 -0300

    footer inicial

--- src/app/pages/pagina-inicial/pagina-inicial.component.ts
+++ src/app/pages/pagina-inicial/pagina-inicial.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { BannerComponent } from '../../components/banner/banner.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-pagina-inicial',
-  imports: [ HeaderComponent, BannerComponent],
+  imports: [ HeaderComponent, BannerComponent, FooterComponent],
   templateUrl: './pagina-inicial.component.html',
   styleUrls: ['./pagina-inicial.component.scss']
 })
