950d323f105d42c8fa2a4cd73248bfbdd65c1b88
gabriel-castro <gabrielcastro.developer@gmail.com>
2025-09-21 23:21:50 -0300

    botões card de produto

--- src/app/components/produtos/produto-card/produto-card.component.ts
+++ src/app/components/produtos/produto-card/produto-card.component.ts
@@ -1,12 +1,16 @@
 import { Component, Input } from '@angular/core';
 import { Produto } from '../../../model/produto.model';
+import { AuthService } from '../../../service/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-produto-card',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './produto-card.component.html',
   styleUrl: './produto-card.component.scss'
 })
 export class ProdutoCardComponent {
   @Input() produto!: Produto;
+
+  constructor(public authService: AuthService) {}
 }
