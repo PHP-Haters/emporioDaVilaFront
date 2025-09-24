d32dcd66a542c3555a8547394f601fc04ef8a4d1
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-09-24 12:59:43 -0300

    listagem de cateforias no modal de edição de produto

--- src/app/components/produtos/edit-produto-modal/edit-produto-modal.component.ts
+++ src/app/components/produtos/edit-produto-modal/edit-produto-modal.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MdbModalRef } from 'mdb-angular-ui-kit/modal';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ProdutosService } from '../../../service/produtos/produtos.service';
 
 @Component({
   selector: 'app-edit-produto-modal',
@@ -16,15 +17,29 @@ export class EditProdutoModalComponent {
 
   constructor(
     public modalRef: MdbModalRef<EditProdutoModalComponent>,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private produtosService: ProdutosService
   ) {}
 
   ngOnInit(): void {
+    this.configForm();
+    this.loadCategories();
+  }
+
+  configForm(): void {
     this.form = this.fb.group({
       nome: ['', Validators.required],
       valor: [0, [Validators.required, Validators.min(0)]],
       stock: [false],
       categoria: ['', Validators.required]
+    });
+  }
+
+  loadCategories(): void {
+    this.produtosService.getCategorias().subscribe({
+      next: (categorias) => {
+        this.categorias = categorias;
+      },
     });
   }
 
