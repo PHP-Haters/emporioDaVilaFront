7774b6a369a72162a26901d4a2ee30ddb949c1d5
Ana Ripoli <anaripoli.eng@gmail.com>
2025-09-25 11:20:41 -0300

    service login

---
+++ src/app/service/login/login.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Produto } from "../../model/produto.model";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class LoginService {
+    url = 'http://localhost:8080/produto';
+    apiUrl: any;
+
+    constructor(private http: HttpClient) {}
+
+    login(email: string, senha: string): Observable<boolean> {
+        return this.http.post<boolean>(`${this.url}/login`, {email, senha});
+    }
+
+    logout(): void {
+        sessionStorage.removeItem('logado');
+    }
+
+    salvarSessao(): void {
+        sessionStorage.setItem('logado', 'true');
+    }
+
+    isLogado(): boolean {
+        return sessionStorage.getItem('logado') === 'true';
+    }
+}
