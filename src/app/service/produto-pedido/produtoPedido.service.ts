ee55503e1109b2697588b673414fe10295a43a0f
Ana Ripoli <anaripoli.eng@gmail.com>
2025-09-29 16:23:15 -0300

    service pedido produto

---
+++ src/app/service/produto-pedido/produtoPedido.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { ProdutoPedido } from "../../model/produto-pedido.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdutoPedidoService {
+  private url = 'http://localhost:8080/produto-pedido';
+
+  constructor(private http: HttpClient) {}
+
+  criarProdutoPedido(produtoPedido: ProdutoPedido): Observable<any> {
+    return this.http.post(this.url, produtoPedido, { responseType: 'text' });
+  }
+
+  getAllProdutoPedidos(): Observable<ProdutoPedido[]> {
+    return this.http.get<ProdutoPedido[]>(this.url);
+  }
+
+  getProdutoPedidoById(id: number): Observable<ProdutoPedido> {
+    return this.http.get<ProdutoPedido>(`${this.url}/${id}`);
+  }
+
+  editarProdutoPedido(produtoPedido: ProdutoPedido): Observable<any> {
+    return this.http.put(`${this.url}/${produtoPedido.id}`, produtoPedido, { responseType: 'text' });
+  }
+
+  deleteProdutoPedido(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
