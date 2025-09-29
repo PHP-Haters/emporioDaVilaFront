26cecad5f62b78c2694134a89a7ccba6eb7683b7
Ana Ripoli <anaripoli.eng@gmail.com>
2025-09-29 15:50:29 -0300

    service usuario

---
+++ src/app/service/usuario/usuario.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Usuario } from "../../model/usuario.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+  private url = 'http://localhost:8080/usuario';
+
+  constructor(private http: HttpClient) {}
+
+  criarUsuario(usuario: Usuario): Observable<any> {
+    return this.http.post(this.url, usuario, { responseType: 'text' });
+  }
+
+  getAllUsuarios(): Observable<Usuario[]> {
+    return this.http.get<Usuario[]>(this.url);
+  }
+
+  getUsuarioById(id: number): Observable<Usuario> {
+    return this.http.get<Usuario>(`${this.url}/${id}`);
+  }
+
+  editarUsuario(usuario: Usuario): Observable<any> {
+    return this.http.put(`${this.url}/${usuario.id}`, usuario, { responseType: 'text' });
+  }
+
+  deleteUsuario(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+}
