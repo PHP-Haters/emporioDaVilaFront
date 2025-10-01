b019b40a119e069ac99db43976e0b47878c722a1
Ana Ripoli <anaripoli.eng@gmail.com>
2025-10-01 20:25:39 -0300

    inicio pagina contato

--- src/app/pages/pagina-contato/pagina-contato.component.ts
+++ src/app/pages/pagina-contato/pagina-contato.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
+import { BannerComponent } from '../../components/banner/banner.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-pagina-contato',
-  imports: [ HeaderComponent ],
+  imports: [ HeaderComponent, BannerComponent, FooterComponent], 
   templateUrl: './pagina-contato.component.html',
-  styleUrl: './pagina-contato.component.scss'
+  styleUrls: ['./pagina-contato.component.scss']
 })
+
 export class PaginaContatoComponent {
 
 }
