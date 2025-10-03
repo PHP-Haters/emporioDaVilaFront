b298daf67843e30771f3ad366f5b2641eed1849d
Guillermo <guillermotoro61@gmail.com>
2025-10-02 22:43:58 -0300

    Login e registro funfando

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -13,16 +13,35 @@ import { Usuario } from '../../model/usuario.model';
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {  
+  
+  dadosLogin = {
+    email: '',
+    senha: ''
+  };
+  
+  // Dados específicos para CADASTRO
+  isRegistering: boolean = false;
+  dadosCadastro = {
+    nome: '',
+    email: '',
+    senha: '',
+    confirmSenha: ''
+  };
 
   mensagem: string = "";
   sucesso: boolean = false;
   usuarioLogado: Usuario = new Usuario;
 
+
   constructor(private router: Router, private authUtil: AuthUtil, private usuarioService: UsuarioService) {}
+
+  toggleMode() {
+    this.isRegistering = !this.isRegistering;
+  }
 
   login(event: Event, email: string, senha: string) {
     event.preventDefault();
-   const usuario: Usuario = new Usuario(); // cria instância de Produto
+   const usuario: Usuario = new Usuario(); // cria instância de Usuario
    
    usuario.email = email;
    usuario.senha = senha;
@@ -39,6 +58,32 @@ export class LoginPageComponent {
    });
   }
 
+  register(event: Event, email: string, senha: string, confirm: string, nome: string, telefone: string) {
+      event.preventDefault();
+      const usuario: Usuario = new Usuario(); // cria instância de Usuario
+      
+      if(senha !== confirm) {
+          this.sucesso = false;
+            this.mensagem = "Senhas diferentes.";
+        return;
+      }
+
+      usuario.email = email;
+      usuario.senha = senha;
+      usuario.nome = nome;
+      usuario.telefone = Number(telefone);
+
+      this.usuarioService.criarUsuario(usuario).subscribe({
+        next:  (user) => {
+            this.usuarioLogado = user;
+            this.authUtil.login(this.usuarioLogado);
+            
+            this.sucesso = true;
+            this.mensagem = "Conta criada!";
+            this.isRegistering = false;
+        }
+      });
+  }
   redirectToIndex() {
     setTimeout(() => {
         this.router.navigate(['inicio']);
