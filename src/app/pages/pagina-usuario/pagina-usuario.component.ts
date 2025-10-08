1610d84de3c0e90ae82178e1026ff7e065429ae1
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-10-08 13:29:40 -0300

    html inicial do formulário de usuário

--- src/app/pages/pagina-usuario/pagina-usuario.component.ts
+++ src/app/pages/pagina-usuario/pagina-usuario.component.ts
@@ -4,34 +4,62 @@ import { FooterComponent } from '../../components/footer/footer.component';
 import { AuthUtil } from '../../utils/auth.util';
 import { UsuarioService } from '../../service/usuario/usuario.service';
 import { Usuario } from '../../model/usuario.model';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-pagina-usuario',
-  imports: [ HeaderComponent, FooterComponent ],
+  imports: [ HeaderComponent, FooterComponent, FormsModule ],
   templateUrl: './pagina-usuario.component.html',
   styleUrl: './pagina-usuario.component.scss'
 })
-export class PaginaUsuarioComponent implements OnInit{
+export class PaginaUsuarioComponent implements OnInit {
   currentUserId?: number;
   loggedUser!: Usuario;
+  showPassword = false; // controla o estado do input
+  telefone: string = '';
 
-  constructor (private authUtil: AuthUtil, private usuarioService: UsuarioService) {}
+  constructor(
+    private authUtil: AuthUtil,
+    private usuarioService: UsuarioService
+  ) {}
 
   ngOnInit(): void {
     this.getUserData();
   }
 
   getUserData() {
-    // Getting user Id
     const id = this.authUtil.loggedUserId;
     this.currentUserId = Number(id);
 
-    // Bringing user data from backend
     this.usuarioService.getUsuarioById(this.currentUserId).subscribe({
       next: (usuario) => {
         console.log(usuario);
-      }
+      },
     });
+  }
+
+  togglePassword(input: HTMLInputElement) {
+    this.showPassword = !this.showPassword;
+    input.type = this.showPassword ? 'text' : 'password';
+  }
+
+  formatTelefone(event: Event) {
+    const input = event.target as HTMLInputElement;
+    let digits = input.value.replace(/\D/g, ''); // remove tudo que não é número
+
+    // limita a 11 dígitos
+    if (digits.length > 11) digits = digits.substring(0, 11);
+
+    if (digits.length > 6) {
+      input.value = `(${digits.substring(0, 2)}) ${digits.substring(
+        2,
+        digits.length - 4
+      )}-${digits.substring(digits.length - 4)}`;
+    } else if (digits.length > 2) {
+      input.value = `(${digits.substring(0, 2)}) ${digits.substring(2)}`;
+    } else if (digits.length > 0) {
+      input.value = `(${digits}`;
+    }
   }
 
 }
