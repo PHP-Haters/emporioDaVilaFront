7e882cd81abbad443860f37ed36e52661fb12c49
Gabriel Castro <gabrielcastro.developer@gmail.com>
2025-10-08 12:37:21 -0300

    consultando usuário por Id

--- src/app/pages/pagina-usuario/pagina-usuario.component.ts
+++ src/app/pages/pagina-usuario/pagina-usuario.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { AuthUtil } from '../../utils/auth.util';
+import { UsuarioService } from '../../service/usuario/usuario.service';
+import { Usuario } from '../../model/usuario.model';
 
 @Component({
   selector: 'app-pagina-usuario',
@@ -10,22 +12,26 @@ import { AuthUtil } from '../../utils/auth.util';
   styleUrl: './pagina-usuario.component.scss'
 })
 export class PaginaUsuarioComponent implements OnInit{
-  userId?: number;
+  currentUserId?: number;
+  loggedUser!: Usuario;
 
-  constructor (private authUtil: AuthUtil) {}
+  constructor (private authUtil: AuthUtil, private usuarioService: UsuarioService) {}
 
   ngOnInit(): void {
     this.getUserData();
   }
 
   getUserData() {
+    // Getting user Id
     const id = this.authUtil.loggedUserId;
-    if (id) {
-      this.userId = Number(id);
-      console.log(this.userId);
-    } else {
-      console.warn("Nenhum usuário logado.");
-    }
+    this.currentUserId = Number(id);
+
+    // Bringing user data from backend
+    this.usuarioService.getUsuarioById(this.currentUserId).subscribe({
+      next: (usuario) => {
+        console.log(usuario);
+      }
+    });
   }
 
 }
